f72155b60f415b46adc6a7502fa53a1bb840a6fa
Abel N. Nieva <931497+abelnnieva@users.noreply.github.com>
2021-07-01 21:57:57 +0200

    feat: database ssl conf

--- config/database.js
+++ config/database.js
@@ -5,12 +5,18 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          ca:
+            process.env.NODE_ENV === 'production'
+              ? process.env.CA_CERT
+              : fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString(),
+        },
       },
       options: {}
     },
